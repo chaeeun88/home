9f606eb7d969e98501fe70c0bbdef5af060ceb07
1233546 <127706200+chaeeun88@users.noreply.github.com>
2023-04-10 04:05:25 +0900

    Add files via upload

---
+++ Untitled-1.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$n = 30;
+$sum = 0;
+$prod = 1;
+for($x =1; $x <= $n; $x+=1){
+    $sum = sum + x;
+    $prod = prod * x;
+    echo "$x ";
+}
+echo "<br>1+...+n = $sum<br>";
+echo "1*...*n = $prod<br>";
+
+?>
+</body>
+</html>
